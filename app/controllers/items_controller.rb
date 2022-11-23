4fb063e2e79029879ffcb69c7a800b7045b3b312
Bambam320 <igor.mestechkin@gmail.com>
2022-11-22 23:34:17 -0500

    Completed Assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
   def index
-    items = Item.all
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create 
+    puts "these are params", params
+    user = User.find(params[:user_id])
+    new_item = user.items.create(items_params)
+    render json: new_item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { errors: "Not Found" }, status: :not_found
+  end
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 end
